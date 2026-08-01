c8823d697ff06248a3a250e9c7e9820a441a3ee5
Aurelien <barbotin48@gmail.com>
2026-08-01 18:39:45 +0200

    fix: correct Netlify asset base path

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite'
 
+export default defineConfig({
+  base: '/',
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/front/' : '/',
-  plugins: [vue(), tailwindcss()],
   test: {
-    environment: "jsdom",
+    environment: 'jsdom',
     globals: true,
   },
 
   resolve: {
     alias: {
-      "@": "/src",
+      '@': '/src',
     },
   },
-}))
+})
